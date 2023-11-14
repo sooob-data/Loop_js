6540843efac3fe2cf2daacc425feb4e1821c10d9
Shubham yadav <149668175+sooob-data@users.noreply.github.com>
2023-11-14 23:08:40 +0000

    file_attched

---
+++ script.js
@@ -0,0 +1,25 @@
+console.clear()
+//fuction loop
+function table(num){
+    for (let i = 1; i <= 10; i++){
+        if (i ===3) {
+            continue;
+        }
+
+        console.log(`${num} x ${i} = ${num*i}`);
+    }
+}
+table(2);
+
+console.log("Reverse")
+
+function reverse(num1){
+    for (let i = 10; i >= 1; i--){
+        if (i <= 3) {
+            break;
+        }
+
+        console.log(`${num1} x ${i} = ${num1*i}`);
+    }
+}
+reverse(5);
